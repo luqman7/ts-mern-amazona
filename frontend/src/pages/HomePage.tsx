2673002c0a3c117d42c0e3a3f4e1fa8f80a83188
luqman <muhamadluqmanhakim69@gmail.com>
2023-04-21 00:17:25 +0800

    setup routing

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { Col, Row } from "react-bootstrap"
+import { sampleProduct } from "../data"
+import { Link } from "react-router-dom"
+
+export default function HomePage() {
+  return (
+    <Row>
+      {sampleProduct.map((product) => (
+        <>
+          <Col key={product.slug} sm={6} md={4} lg={3}>
+            <Link to={"/product/" + product.slug}>
+              <h2>{product.name}</h2>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <p>RM {product.price}</p>
+            </Link>
+          </Col>
+        </>
+      ))}
+    </Row>
+  )
+}
