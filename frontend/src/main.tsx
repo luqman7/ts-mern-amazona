6dcad24ea73d6e000300f5fe06acb2118f212681
luqman <muhamadluqmanhakim69@gmail.com>
2023-04-21 18:22:10 +0800

    fetch data using react query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,11 +11,9 @@ import "./index.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import HomePage from "./pages/HomePage"
 import ProductPage from "./pages/ProductPage"
-import axios from "axios"
 import { HelmetProvider } from "react-helmet-async"
-
-axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:4000" : "/"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -28,10 +26,15 @@ const router = createBrowserRouter(
   )
 )
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <HelmetProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </HelmetProvider>
   </React.StrictMode>
 )
