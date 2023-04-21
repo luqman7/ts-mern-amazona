9b6e38bb742368d9796de49a9c9f5b73d5b59ffd
luqman <muhamadluqmanhakim69@gmail.com>
2023-04-21 19:06:53 +0800

    create product page

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,11 @@ app.use(
 app.get("/api/products", (req: Request, res: Response) => {
   res.json(sampleProduct)
 })
+
+app.get("/api/products/:slug", (req: Request, res: Response) => {
+  res.json(sampleProduct.find((x) => x.slug === req.params.slug))
+})
+
 const PORT = 4000
 app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`)
