a74f2b626b2041e8fb8a658b5e5e8d854c809d07
luqman <muhamadluqmanhakim69@gmail.com>
2023-05-02 00:10:05 +0800

    in progress: implement add to cart

--- frontend/src/Store.tsx
+++ frontend/src/Store.tsx
@@ -1,7 +1,9 @@
 import React from "react"
+import { Cart, CartItem } from "./types/Cart"
 
 type AppState = {
   mode: string
+  cart: Cart
 }
 
 const initialState: AppState = {
@@ -11,14 +13,31 @@ const initialState: AppState = {
       window.matchMedia("(prefers-color-scheme: dark)").matches
     ? "dark"
     : "light",
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems")!)
+      : [],
+    shippingAddress: localStorage.getItem("shippingAddress")
+      ? JSON.parse(localStorage.getItem("shippingAddress")!)
+      : {},
+    paymentMethod: localStorage.getItem("paymentMethod")
+      ? localStorage.getItem("paymentMethod")!
+      : "PayPal",
+    itemsPrice: 0,
+    shippingPrice: 0,
+    taxPrice: 0,
+    totalPrice: 0,
+  },
 }
 
-type Action = { type: "SWITCH_MODE" }
+type Action =
+  | { type: "SWITCH_MODE" }
+  | { type: "CART_ADD_ITEM"; payload: CartItem }
 
 function reducer(state: AppState, action: Action): AppState {
   switch (action.type) {
     case "SWITCH_MODE":
-      return { mode: state.mode === "dark" ? "light" : "dark" }
+      return { ...state, mode: state.mode === "dark" ? "light" : "dark" }
     default:
       return state
   }
